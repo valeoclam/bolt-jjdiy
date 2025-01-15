22e01393f2fe0dd56f1da5df274d807396d992a6
valeoclam <jerrylu0505@yahoo.com>
2025-01-15 14:45:05 -0800

    Initial commit from your app

--- src/LazyDiaryPage.jsx
+++ src/LazyDiaryPage.jsx
@@ -476,45 +476,11 @@ const handleStartRecording = async () => {
 
 
 const handleStopRecording = () => {
-  return new Promise((resolve) => {
-    if (mediaRecorder) {
-      console.log("handleStopRecording - mediaRecorder state:", mediaRecorder.state);
-      mediaRecorder.onstop = () => {
-        console.log("handleStopRecording - onstop - mediaRecorder.chunks:", mediaRecorder.chunks);
-        const blob = new Blob(chunks, { type: 'audio/mp4' });
-        console.log("handleStopRecording - onstop - blob:", blob);
-        console.log("handleStopRecording - onstop - blob.size:", blob.size);
-        console.log("handleStopRecording - onstop - blob.type:", blob.type);
-        setAudioBlob(blob);
-        setAudioUrl(URL.createObjectURL(blob));
-        setTempAudioBlob(blob);
-        setTempAudioUrl(URL.createObjectURL(blob));
-        chunks = []; // Clear chunks after creating blob
-        resolve();
-      };
-      try {
-        if (mediaRecorder.state !== 'inactive') {
-          mediaRecorder.stop();
-          setIsRecording(false);
-          setRecordButtonText('开始录音');
-          setTimeout(() => {
-            resolve();
-          }, 100);
-        } else {
-          setIsRecording(false);
-          setRecordButtonText('开始录音');
-          resolve();
-        }
-      } catch (error) {
-        console.error("handleStopRecording - mediaRecorder.stop() error:", error);
-        setIsRecording(false);
-        setRecordButtonText('开始录音');
-        resolve();
-      }
-    } else {
-      resolve();
-    }
-  });
+  if (mediaRecorder) {
+    mediaRecorder.stop();
+    setIsRecording(false);
+    setRecordButtonText('开始录音');
+  }
 };
 
 
