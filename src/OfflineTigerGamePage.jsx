37bb367f2007ca397f814b718f5c5d7ebb2ae4d4
valeoclam <jerrylu0505@yahoo.com>
2025-01-29 15:35:03 -0800

    Update OfflineTigerGamePage.jsx

--- src/OfflineTigerGamePage.jsx
+++ src/OfflineTigerGamePage.jsx
@@ -3,11 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 import * as EXIF from 'exifreader';
 import imageCompression from 'browser-image-compression';
 import { useNavigate } from 'react-router-dom';
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://fhcsffagxchzpxouuiuq.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoY3NmZmFneGNoenB4b3V1aXVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYyMTQzMzAsImV4cCI6MjA1MTc5MDMzMH0.1DMl870gjGRq5LRlQMES9WpYWehiKiPIea2Yj1q4Pz8';
-const supabase = createClient(supabaseUrl, supabaseKey);
+import supabase from './supabaseClient';
 
 function OfflineTigerGamePage({ onLogout }) {
   const [inputAmount, setInputAmount] = useState('');
