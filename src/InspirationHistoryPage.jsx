1901bf44b8482d04cbd84141eb312561d97575cd
valeoclam <jerrylu0505@yahoo.com>
2025-01-29 15:32:34 -0800

    Update InspirationHistoryPage.jsx

--- src/InspirationHistoryPage.jsx
+++ src/InspirationHistoryPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import imageCompression from 'browser-image-compression';
+import supabase from './supabaseClient';
 
 function InspirationHistoryPage({ loggedInUser, supabase, onLogout }) {
   const [inspirations, setInspirations] = useState([]);
