fb1eef6169b1651d1b27e5842003fc826b84ae88
valeoclam <jerrylu0505@yahoo.com>
2025-01-29 15:29:08 -0800

    Add files via upload

---
+++ src/supabaseClient.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+    const supabaseUrl = 'https://fhcsffagxchzpxouuiuq.supabase.co';
+    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoY3NmZmFneGNoenB4b3V1aXVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYyMTQzMzAsImV4cCI6MjA1MTc5MDMzMH0.1DMl870gjGRq5LRlQMES9WpYWehiKiPIea2Yj1q4Pz8';
+
+    const supabase = createClient(supabaseUrl, supabaseKey);
+
+    export default supabase;
