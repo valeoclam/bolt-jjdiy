4390bb0696d88730a2b120ac6416080c091195c2
valeoclam <jerrylu0505@yahoo.com>
2025-01-29 15:33:51 -0800

    Update LazyDiaryPage.jsx

--- src/LazyDiaryPage.jsx
+++ src/LazyDiaryPage.jsx
@@ -1,11 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { createClient } from '@supabase/supabase-js';
 import imageCompression from 'browser-image-compression';
-
-const supabaseUrl = 'https://fhcsffagxchzpxouuiuq.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoY3NmZmFneGNoenB4b3V1aXVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYyMTQzMzAsImV4cCI6MjA1MTc5MDMzMH0.1DMl870gjGRq5LRlQMES9WpYWehiKiPIea2Yj1q4Pz8';
-const supabase = createClient(supabaseUrl, supabaseKey);
+import supabase from './supabaseClient';
 
 function LazyDiaryPage({ loggedInUser, onLogout }) {
     const [currentQuestion, setCurrentQuestion] = useState('');
