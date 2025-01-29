81c9bf332e912043cf08a83e654b9d873486ea40
valeoclam <jerrylu0505@yahoo.com>
2025-01-29 15:34:48 -0800

    Update OfflinePaymentMultiplierCalculator.jsx

--- src/OfflinePaymentMultiplierCalculator.jsx
+++ src/OfflinePaymentMultiplierCalculator.jsx
@@ -1,10 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://fhcsffagxchzpxouuiuq.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoY3NmZmFneGNoenB4b3V1aXVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYyMTQzMzAsImV4cCI6MjA1MTc5MDMzMH0.1DMl870gjGRq5LRlQMES9WpYWehiKiPIea2Yj1q4Pz8';
-const supabase = createClient(supabaseUrl, supabaseKey);
+import supabase from './supabaseClient';
 
 function OfflinePaymentMultiplierCalculator() {
   const [records, setRecords] = useState([]);
