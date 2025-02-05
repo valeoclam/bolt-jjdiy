054cad3a38bad8ae6674c3db2c9d983d157bb529
valeoclam <jerrylu0505@yahoo.com>
2025-02-05 10:38:17 -0800

    Update TigerGamePage.jsx

--- src/TigerGamePage.jsx
+++ src/TigerGamePage.jsx
@@ -134,7 +134,7 @@ useEffect(() => {
 
     try {
       const compressedFile = await imageCompression(file, {
-        maxSizeMB: 0.05,
+        maxSizeMB: 0.5,
         maxWidthOrHeight: 150,
         useWebWorker: true,
       });
@@ -159,7 +159,7 @@ useEffect(() => {
       const compressedFiles = await Promise.all(
         files.map(async (file) => {
           return await imageCompression(file, {
-            maxSizeMB: 0.05,
+            maxSizeMB: 0.5,
             maxWidthOrHeight: 150,
             useWebWorker: true,
           });
